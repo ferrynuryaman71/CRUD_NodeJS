d1523ab5a4b1c723e59b3f69102f5f7013a4b1c9
ferry <ferrynuryaman01@gmail.com>
2019-02-05 17:08:07 +0700

    add update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+const client = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: true });
+
+	  client.connect((err, db) => {
+	if (err) {
+		return console.log('unable to connect to server');
+	}
+		console.log('connect to server');
+	db = db.db('TodoApp');
+
+	//findOneAndUpdate
+	db.collection('User').findOneAndUpdate({
+		_id	: 1
+	}, {
+	  $set: {
+	  	nama	: 'Ferry'
+	  }
+	}, {
+		returnOriginal : false
+	}).then((result) => {
+		console.log(result)
+	})
+
+	client.close();
+});
