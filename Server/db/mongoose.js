3bc21fad72de73e92ec5a4682632c3bcf7f6ead3
ferry <ferrynuryaman01@gmail.com>
2019-02-11 10:10:39 +0700

    setup heroku

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
-mongoose.promise = global.promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-
-module.exports = {mongoose}
+module.exports = {mongoose};
--- Server/server.js
+++ Server/server.js
@@ -22,12 +22,13 @@ app.post('/todos', (req, res) => {
 	});
 });
 
-app.get('/todos/:id', (req, res) => {
-	Todo.find().then((todos) => {
-		res.send({todos})
-	}, (e) => {
-		res.status(400).send(e);
-	});	
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
@@ -48,6 +49,7 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
 
 
 app.listen(port, () => {
